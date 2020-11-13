515786a98881107afdce23211f632f89ead7298d
stdevAdrianPaez <adriantpaez@gmail.com>
2020-11-13 17:11:00 +0100

    Initial implementation

---
+++ Makefile
@@ -0,0 +1,14 @@
+# ======
+# COLORS
+# ======
+
+NO_COLOR=\033[0m
+YELLOW_COLOR=\033[1;33m
+
+# =====
+# BUILD
+# =====
+
+build:
+	@echo "$(YELLOW_COLOR)=====\nBUILD\n=====$(NO_COLOR)"
+	go build -o uh-email-quota.bin -i src/main.go
---
+++ src/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+)
+
+type QuotaInfo struct {
+	Value      int
+	Limit      int
+	Percentage float64
+}
+
+func ParseQuotaInfo(line_str string) (*QuotaInfo, error) {
+	lines := strings.Split(line_str, "\n")
+	if len(lines) < 2 {
+		return nil, errors.New("unexpected output format")
+	}
+	// Check header
+	if lines[0] != "Quota name\tType\tValue\tLimit\t%" {
+		return nil, errors.New("unexpected output format")
+	}
+
+	// Parse info
+	words := strings.Split(lines[1], "\t")
+	if len(words) < 5 {
+		return nil, errors.New("unexpected output format")
+	}
+
+	value, err := strconv.Atoi(words[3])
+	if err != nil {
+		return nil, errors.New("unexpected output format in Value field")
+	}
+	limnit, err := strconv.Atoi(words[4])
+	if err != nil {
+		return nil, errors.New("unexpected output format in Limit field")
+	}
+	percentage, err := strconv.ParseFloat(words[5], 64)
+	if err != nil {
+		return nil, errors.New("unexpected output format in % field")
+	}
+	return &QuotaInfo{
+		Value:      value,
+		Limit:      limnit,
+		Percentage: percentage,
+	}, nil
+}
+
+func get_user_quota(user_name string) (*QuotaInfo, error) {
+	cmd := exec.Command("doveadm", "-f", "tab", "quota", "get", "-u", user_name)
+	fmt.Println(cmd.String())
+	cmd.Stderr = os.Stderr
+	out, err := cmd.Output()
+	if err != nil {
+		return nil, err
+	}
+	if err := cmd.Run(); err != nil {
+		return nil, err
+	}
+	out_str := string(out)
+	
+	return ParseQuotaInfo(out_str)
+
+}
+
+func main() {
+	user := os.Args[1]
+	quota_info, err := get_user_quota(user)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(quota_info)
+}
